4d5941022b7ed83bdbce11f88fef579df77c009f
runner <runner@Mac-1641359503609.local>
2022-01-05 05:28:05 +0000

    tencentyun/iot-p2p-build@ff2d754

--- Source/XP2P-iOS/Classes/AppWrapper.h
+++ Source/XP2P-iOS/Classes/AppWrapper.h
@@ -11,7 +11,7 @@ extern "C" {
 
 #define MAX_SIZE_OF_PARAMS 3000
 
-static const char * VIDEOSDKVERSION = "2.4.x+git.7ba01e30";
+static const char * VIDEOSDKVERSION = "2.3.x+git.883c86e0";
 
 typedef enum
 {
