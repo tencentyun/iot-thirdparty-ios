f576874954884a3e8404132678dc82ad7c3b8bb9
runner <runner@Mac-1650265909947.local>
2022-04-18 08:03:39 +0000

    tencentyun/iot-p2p-build@e43ad62

--- Source/XP2P-iOS/Classes/AppWrapper.h
+++ Source/XP2P-iOS/Classes/AppWrapper.h
@@ -9,19 +9,9 @@ extern "C" {
 #include <stddef.h>
 #include <stdint.h>
 
-#ifdef WINDOWS
-#ifdef IPCLIBRARY_EXPORTS
-#  define IPCLIBRARY_API __declspec(dllexport)
-#else
-#  define IPCLIBRARY_API __declspec(dllimport)
-#endif
-#else
-#  define IPCLIBRARY_API
-#endif
-
 #define MAX_SIZE_OF_PARAMS 3000
 
-static const char * VIDEOSDKVERSION = "2.4.x+git.af387a96";
+static const char * VIDEOSDKVERSION = "2.3.x+git.82668bed";
 
 typedef enum
 {
@@ -34,7 +24,6 @@ typedef enum
     XP2PTypeDeviceMsgArrived = 1006,  // 设备端向App发消息
     XP2PTypeCmdNOReturn      = 1007,  // 设备未返回app自定义信令
     XP2PTypeStreamEnd        = 1008,  // 设备停止推流，或者由于达到设备最大连接数，拒绝推流
-    XP2PTypeDownloadEnd      = 1009,  // 设备停止推流，下载结束
     XP2PTypeSaveFileOn       = 8000,  //获取保存音视频流开关状态
     XP2PTypeSaveFileUrl      = 8001   //获取音视频流保存路径
 } XP2PType;
@@ -83,7 +72,7 @@ typedef char *(*device_data_recv_handle_t)(const char *id, uint8_t *recv_buf, si
  * @param device_data_handle: 设备端向App发送消息的回调
  * @return 无返回值
  */
-IPCLIBRARY_API void setUserCallbackToXp2p(av_recv_handle_t recv_handle, msg_handle_t msg_handle, device_data_recv_handle_t device_data_handle);
+void setUserCallbackToXp2p(av_recv_handle_t recv_handle, msg_handle_t msg_handle, device_data_recv_handle_t device_data_handle);
 
 /**
  * @brief 发送信令消息给camera设备并等待回复，同步阻塞方式
@@ -98,7 +87,7 @@ IPCLIBRARY_API void setUserCallbackToXp2p(av_recv_handle_t recv_handle, msg_hand
  * @param timeout_us: 命令超时时间，单位为微秒，值为0时采用默认超时(7500ms左右)
  * @return 0 为成功
  */
-IPCLIBRARY_API int postCommandRequestSync(const char *id, const unsigned char *command, size_t cmd_len,
+int postCommandRequestSync(const char *id, const unsigned char *command, size_t cmd_len,
                            unsigned char **recv_buf, size_t *recv_len, uint64_t timeout_us);
 
 /**
@@ -110,7 +99,7 @@ IPCLIBRARY_API int postCommandRequestSync(const char *id, const unsigned char *c
  * @param cmd_len: command长度
  * @return 0 为成功
  */
-IPCLIBRARY_API int postCommandRequestWithAsync(const char *id, const unsigned char *command, size_t cmd_len);
+int postCommandRequestWithAsync(const char *id, const unsigned char *command, size_t cmd_len);
 
 /**
  * @brief 向camera设备请求媒体流，异步回调方式
@@ -120,7 +109,7 @@ IPCLIBRARY_API int postCommandRequestWithAsync(const char *id, const unsigned ch
  * @param crypto: 是否开启传输层加密，如果关闭(crypto=false)，则建议用户在应用层加密，否则有安全风险
  * @return 请求句柄
  */
-IPCLIBRARY_API void *startAvRecvService(const char *id, const char *params, bool crypto);
+void *startAvRecvService(const char *id, const char *params, bool crypto);
 
 /**
  * @brief 关闭媒体流传输
@@ -129,7 +118,7 @@ IPCLIBRARY_API void *startAvRecvService(const char *id, const char *params, bool
  * @param req: 接口`startAvRecvService`的返回值，当前版本可传入null
  * @return 0 为成功
  */
-IPCLIBRARY_API int stopAvRecvService(const char *id, void *req);
+int stopAvRecvService(const char *id, void *req);
 
 /**
  * @brief 初始化xp2p服务
@@ -139,7 +128,7 @@ IPCLIBRARY_API int stopAvRecvService(const char *id, void *req);
  * @param device_name: 设备名称
  * @return 0 为成功
  */
-IPCLIBRARY_API int startService(const char *id, const char *product_id, const char *device_name);
+int startService(const char *id, const char *product_id, const char *device_name);
 
 /**
  * @brief 初始化xp2p服务
@@ -151,20 +140,20 @@ IPCLIBRARY_API int startService(const char *id, const char *product_id, const ch
  * @param remote_port: 局域网设备端口
  * @return 0 为成功
  */
-IPCLIBRARY_API int startLanService(const char *id, const char *product_id, const char *device_name,
+int startLanService(const char *id, const char *product_id, const char *device_name,
                     const char *remote_host, const char *remote_port);
 
 /**
  * 延迟设置xp2pinfo,节省start时间
  */
-IPCLIBRARY_API int setDeviceXp2pInfo(const char *id, const char *xp2p_info);
+int setDeviceXp2pInfo(const char *id, const char *xp2p_info);
 /**
  * @brief 获取本地代理url
  *
  * @param id: 目标camera在app端的唯一标识符
  * @return 本地代理url
  */
-IPCLIBRARY_API const char *delegateHttpFlv(const char *id);
+const char *delegateHttpFlv(const char *id);
 
 /**
  * @brief 获取局域网url
@@ -172,7 +161,7 @@ IPCLIBRARY_API const char *delegateHttpFlv(const char *id);
  * @param id: 目标camera在app端的唯一标识符
  * @return 局域网url
  */
-IPCLIBRARY_API const char *getLanUrl(const char *id);
+const char *getLanUrl(const char *id);
 
 /**
  * @brief 获取局域网url
@@ -180,7 +169,7 @@ IPCLIBRARY_API const char *getLanUrl(const char *id);
  * @param id: 目标camera在app端的唯一标识符
  * @return 局域网本地代理端口号
  */
-IPCLIBRARY_API int getLanProxyPort(const char *id);
+int getLanProxyPort(const char *id);
 
 /**
  * @brief 启动向camera设备发送语音或自定义数据服务，异步非阻塞方式
@@ -191,7 +180,7 @@ IPCLIBRARY_API int getLanProxyPort(const char *id);
  * @param crypto: 是否开启传输层加密，如果关闭(crypto=false)，则建议用户在应用层加密，否则有安全风险
  * @return 请求句柄
  */
-IPCLIBRARY_API void *runSendService(const char *id, const char *params, bool crypto);
+void *runSendService(const char *id, const char *params, bool crypto);
 
 /**
  * @brief 关闭语音发送传输
@@ -200,7 +189,7 @@ IPCLIBRARY_API void *runSendService(const char *id, const char *params, bool cry
  * @param req: 接口`runSendService`的返回值，当前版本可传入null
  * @return 请求句柄
  */
-IPCLIBRARY_API int stopSendService(const char *id, void *req);
+int stopSendService(const char *id, void *req);
 
 /**
  * @brief 设置云api信息，若不用sdk获取xp2p信息，该接口可不用设置
@@ -209,7 +198,7 @@ IPCLIBRARY_API int stopSendService(const char *id, void *req);
  * @param sec_key: 云API secrct_key
  * @return 0 为成功
  */
-IPCLIBRARY_API int setQcloudApiCred(const char *sec_id, const char *sec_key);
+int setQcloudApiCred(const char *sec_id, const char *sec_key);
 
 /**
  * @brief 向camera设备发送语音或自定义数据
@@ -219,7 +208,7 @@ IPCLIBRARY_API int setQcloudApiCred(const char *sec_id, const char *sec_key);
  * @param len: 要发送的数据长度
  * @return 0 为成功
  */
-IPCLIBRARY_API int dataSend(const char *id, uint8_t *data, size_t len);
+int dataSend(const char *id, uint8_t *data, size_t len);
 
 /**
  * @brief 停止xp2p服务
@@ -227,15 +216,7 @@ IPCLIBRARY_API int dataSend(const char *id, uint8_t *data, size_t len);
  * @param id: 目标camera在app端的唯一标识符
  * @return 0 为成功
  */
-IPCLIBRARY_API void stopService(const char *id);
-
-/**
- * @brief 日志开关
- *
- * @param console: 是否输出日志到控制台
- * @param file: 是否输出日志到文件
- */
-IPCLIBRARY_API void setLogEnable(bool console, bool file);
+void stopService(const char *id);
 
 /*================================废弃接口=======================================*/
 /**
@@ -248,7 +229,7 @@ IPCLIBRARY_API void setLogEnable(bool console, bool file);
  * @param timeout_us: 命令超时时间，单位为微秒，值为0时采用默认超时(7500ms左右)
  * @return 0 为成功
  */
-IPCLIBRARY_API int getCommandRequestWithSync(const char *id, const char *command, char **buf, size_t *len,
+int getCommandRequestWithSync(const char *id, const char *command, char **buf, size_t *len,
                               uint64_t timeout_us);
 
 /**
@@ -258,24 +239,14 @@ IPCLIBRARY_API int getCommandRequestWithSync(const char *id, const char *command
  * @param command:字符格式命令
  * @return 0 为成功
  */
-IPCLIBRARY_API int getCommandRequestWithAsync(const char *id, const char *command);
+int getCommandRequestWithAsync(const char *id, const char *command);
 
-
-
-/**
- * @brief 获取当前发送链路的连接模式：0 无效；62 直连；63 转发
- *
- * @param id:目标camera在app端的唯一标识符
- * @param command:字符格式命令
- * @return 0 为成功
- */
-IPCLIBRARY_API int getStreamLinkMode(const char *id);
 
 /**
  * @brief 调试接口，便于通过播放器请求httpflv时，保存拉取的原始流数据就行分析
  * @param id:目标camera在app端的唯一标识符
  */
-IPCLIBRARY_API void startRecordPlayerStream(const char *id);
+void startRecordPlayerStream(const char *id);
 
 #ifdef __cplusplus
 }
