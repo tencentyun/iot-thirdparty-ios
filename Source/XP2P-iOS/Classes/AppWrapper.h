4ff301543eaf904618c5c99abed0229999a4d07b
runner <runner@Mac-1697714213089.local>
2023-10-19 11:46:31 +0000

    tencentyun/iot-p2p-build@039271d

--- Source/XP2P-iOS/Classes/AppWrapper.h
+++ Source/XP2P-iOS/Classes/AppWrapper.h
@@ -21,7 +21,7 @@ extern "C" {
 
 #define MAX_SIZE_OF_PARAMS 3000
 
-static const char * VIDEOSDKVERSION = "2.4.x+git.244808de";
+static const char * VIDEOSDKVERSION = "2.4.x+git.c68c6b82";
 
 typedef enum
 {
@@ -140,6 +140,7 @@ IPCLIBRARY_API int stopAvRecvService(const char *id, void *req);
  * @return 0 为成功
  */
 IPCLIBRARY_API int startService(const char *id, const char *product_id, const char *device_name);
+IPCLIBRARY_API int startServiceWithMode(const char *id, const char *product_id, const char *device_name, bool udpmode);
 
 /**
  * @brief 初始化xp2p服务
