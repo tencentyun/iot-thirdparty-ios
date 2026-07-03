4950b7f515cee9dee6675664f8a4885c6defd4b3
Anka <runner@sjc22-bt147-0f85281b-6b15-467b-81a2-ec8cc26104ff-6E1AC94DE356.local>
2026-07-03 07:44:34 +0000

    tencentyun/iot-p2p-build@53f2f24

--- Source/XP2P-iOS/Classes/AppWrapper.h
+++ Source/XP2P-iOS/Classes/AppWrapper.h
@@ -21,7 +21,7 @@ extern "C" {
 
 #define MAX_SIZE_OF_PARAMS 3000
 
-static const char * VIDEOSDKVERSION = "2.4.x-fixbug02+git.0f17f260";
+static const char * VIDEOSDKVERSION = "2.4.x-cipherV2+git.948b8503";
 
 typedef enum
 {
@@ -101,6 +101,7 @@ typedef struct app_config_t {
   uint64_t  port;
   XP2PProtocolType type;
   bool cross;
+  char peer_name_crypto[128];
 } app_config_t;
 
 typedef const char *(*msg_handle_t)(const char *id, XP2PType type, const char *msg);
